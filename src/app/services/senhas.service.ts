fbc7024086c54ce77866daad80fd4ab34c5c5b8e
Giovana <gicaalopes2003@gmail.com>
2024-04-15 15:16:57 -0300

    cleaning code by removing console.log

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -17,13 +17,14 @@ export class SenhasService {
   public senhasFaltandoChamar: any[] = [];
   public senhasChamadas: any[] = [];
   public relatorioSenhasAtendidas: any[] = [];
-
+  
   // method to add new password to password array
   senhaRetirada(tipoSenha: string = '') {
     let senhaRetiradaObjeto = {
       icon: '',
       color: '',
       senha: '',
+      tipo_senha: '',
       tm_geracao: new Date().toISOString(),
     };
 
@@ -38,6 +39,7 @@ export class SenhasService {
         (this.senhasArray['SG'].length + 1).toString().padStart(2, '0');
       senhaRetiradaObjeto.icon = 'person';
       senhaRetiradaObjeto.color = 'dark';
+      senhaRetiradaObjeto.tipo_senha = 'SG';
       this.senhasArray.SG.push({ ...senhaRetiradaObjeto });
     } else if (tipoSenha == 'SP') {
       this.somaPrior();
@@ -50,6 +52,7 @@ export class SenhasService {
         (this.senhasArray['SP'].length + 1).toString().padStart(2, '0');
       senhaRetiradaObjeto.icon = 'warning';
       senhaRetiradaObjeto.color = 'danger';
+      senhaRetiradaObjeto.tipo_senha = 'SP';
       this.senhasArray.SP.push({ ...senhaRetiradaObjeto });
     } else if (tipoSenha == 'SE') {
       this.somaExame();
@@ -62,40 +65,36 @@ export class SenhasService {
         (this.senhasArray['SE'].length + 1).toString().padStart(2, '0');
       senhaRetiradaObjeto.icon = 'document';
       senhaRetiradaObjeto.color = 'dark';
+      senhaRetiradaObjeto.tipo_senha = 'SE';
       this.senhasArray.SE.push({ ...senhaRetiradaObjeto });
     }
     this.senhasFaltandoChamar.push({ ...senhaRetiradaObjeto });
-    this.displaySenha();
-  }
-
-  displaySenha() {
-    console.log(this.senhasArray);
-    console.log(this.senhasFaltandoChamar);
-  }
+   }
 
   //method to call password
   chamarSenha() {
     if (this.senhasFaltandoChamar.length == 0) {
       alert('Ainda não há senhas no painel de chamada!');
-    }
-    let senhaPrioritariaEncontrada = false;
-    if (this.senhasChamadas.length <= 4) {
-      for (let senha of this.senhasFaltandoChamar) {
-        if (senha.icon === 'warning') {
-          this.senhasChamadas.push(senha);
-          this.removerSenhaDoArray(senha);
-          senhaPrioritariaEncontrada = true;
-          break;
-        }
-      }
-      if (!senhaPrioritariaEncontrada && this.senhasFaltandoChamar.length > 0) {
-        let próximaSenha = this.senhasFaltandoChamar.shift();
-        this.senhasChamadas.push(próximaSenha);
-      }
-      this.displaySenhasAtendidas();
-      this.displaySenha();
     } else {
-      alert('Limpe o painel!');
+      let senhaPrioritariaEncontrada = false;
+      if (this.senhasChamadas.length <= 4) {
+        for (let senha of this.senhasFaltandoChamar) {
+          if (senha.icon === 'warning') {
+            this.senhasChamadas.push(senha);
+            this.removerSenhaDoArray(senha);
+            senhaPrioritariaEncontrada = true;
+            break;
+          }
+        }
+        if (
+          !senhaPrioritariaEncontrada &&
+          this.senhasFaltandoChamar.length > 0
+        ) {
+          let próximaSenha = this.senhasFaltandoChamar.shift();
+          this.senhasChamadas.push(próximaSenha);
+        }
+        this.displaySenhasAtendidas();
+      }
     }
   }
 
@@ -120,28 +119,23 @@ export class SenhasService {
     this.senhasExame++;
     this.senhasTotal++;
   }
-  
+
   //method to count the number of times the password was answered
   displaySenhasAtendidas() {
     this.atendidaGeral = 0;
     this.atendidaPrior = 0;
     this.atendidaExame = 0;
-  
+
     for (let senha of this.senhasChamadas) {
-      console.log('Ícone da senha:', senha.icon);
-      if (senha.icon === 'person') {
+      if (senha.tipo_senha == 'SG') {
         this.atendidaGeral++;
-        console.log('atendidaGeral:', this.atendidaGeral);
-      } else if (senha.icon === 'warning') {
+      } else if (senha.tipo_senha == 'SP') {
         this.atendidaPrior++;
-        console.log('atendidaPrior:', this.atendidaPrior);
       } else {
         this.atendidaExame++;
-        console.log('atendidaExame:', this.atendidaExame);
       }
     }
-    this.relatorioSenhasAtendidas.push({...this.senhasChamadas});
-    console.log(this.relatorioSenhasAtendidas);
+    this.relatorioSenhasAtendidas.push({ ...this.senhasChamadas });
   }
 
   //method for cleaning the window panel
